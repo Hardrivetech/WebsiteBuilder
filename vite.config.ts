f59e333b9cad7f050a35ac1c48959998c2a37b1e
HardriveTech <thardisky@proton.me>
2025-09-10 04:34:02 -0500

    main

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { cloudflareDevProxyVitePlugin } from "@remix-run/cloudflare";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    cloudflareDevProxyVitePlugin(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
